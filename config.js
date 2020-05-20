b0441a6c9df473345895aa82fda96705865b5e63
kirangaud <33742394+kirangaud@users.noreply.github.com>
2020-05-20 14:55:04 +0530

    config file added

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+   'api_url' : 'http://www.kirangaud.com',
+   'aws': {
+        'key': 'AKIAJVGI3T5CGLZOHTCQ',
+        'secret': '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',
+        'region': 'ap-south-1'
+    }
+};
